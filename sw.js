6008c5d4a81281b20a7581edd861fb14a6ce8484
Ysnmlm <ysn10809080@gmail.com>
2026-02-07 06:44:37 +0100

    update service worker cache

--- sw.js
+++ sw.js
@@ -1,20 +1,39 @@
+const CACHE_NAME = "mon-chat-v2";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+// Installation
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("mon-chat").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./app.js"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
+// Activation : supprime les anciens caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch : réseau d'abord, cache en secours
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
